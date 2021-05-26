7b3d6b1cae1f83114c247cf095e06212946e93c8
darkcode01 <j0s3s3gur4@gmail.com>
2021-05-26 11:16:29 -0400

    description about tree

---
+++ tree/tree.py
@@ -0,0 +1,29 @@
+"""
+Tree:
+  -> Son estructuras no lineales.
+  -> En un tree los nodos pueden apuntar a un numero indefinido de elementos.
+  -> No deben de ser circular.
+
+Teoria:
+  * Los elementos seran un Node
+  * Los Nodes pueden apuntar a otros nodes.
+  * Parent Node: Node que apunta a nuestro Node.
+  * Child Node: Nodes a los que apunta nuestro node.
+  * Root Node: No tiene Parent Node.
+  * Leaf Node: No tiene Child Nodes
+
+Propiedades:
+  -> Nivel: distancia del Root Node al Leaf Node.
+  -> Altura/Profundidad: maximo nivel de un Tree
+
+Tipos de Tree:
+  -> Tree Binary
+
+Operaciones:
+  -> insert
+  -> delete
+  -> location
+  -> path
+
+Ojo: Es bueno tener un numero reducido de hijos.
+"""

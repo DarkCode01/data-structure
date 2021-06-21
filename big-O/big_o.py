7c2e62f893c7e372ebde8d3e993cd9318c3a8860
darkcode01 <j0s3s3gur4@gmail.com>
2021-06-21 16:12:25 -0400

    big O and bitwise :D

---
+++ big-O/big_o.py
@@ -0,0 +1,15 @@
+"""
+Lenguaje y metrica que usamos para describir la eficiencia de los
+algoritmos. Algunos ejemplos conocidos de notaciones son:
+
+-> O(1)
+-> O(n): mientras mas datos ingrese el tiempo va aumentando.
+-> O(n^2): mayour cantidad de datos el tiempo se aumenta 2 veces mas
+-> O(2^n): mayour cantidad de datos el tiempo se aumenta (n) veces mas
+-> O(log n): no tiende a aumentar aunque el input sea grande.
+-> O(n log n)
+
+Algoritmos:
+-> recursividad: puede ser O(2^n)
+-> binary search: puede ser O(log n)
+"""

6aea7ef76cbfe41841808bbe850f710564457390
darkcode01 <j0s3s3gur4@gmail.com>
2021-08-02 15:52:16 -0400

    short path

---
+++ graphs/dijkstra.py
@@ -0,0 +1,19 @@
+def dijkstraShortestPathAlgo(nodes, edges, source_indedx = 0):
+    """
+        code from 
+        => [https://www.youtube.com/watch?v=VnTlW572Sc4]
+    """
+    path_lengths = {v:float('inf') for v in nodes}
+    path_lengths[source_index] = 0
+    adjacent_nodes = {v:{} for v in nodes}
+    for (u, v), w_uv in edges.items():
+        adjacent_nodes[u][v] = w_uv
+        adjacent_nodes[v][u] = w_uv
+    temporary_nodes = [v for v in nodes]
+    while len(temporary_nodes) > 0:
+        upper_bounds = {v: path_lengths[v] for v in temporary_nodes}
+        u = min(upper_bounds, key=upper_bounds.get)
+        temporary_nodes.remove(u)
+        for v, w_uv in adjacent_nodes[u].items():
+            path_lengths[v] = min(path_lengths[v], path_lengths[u] + w_uv)
+    return path_lengths
